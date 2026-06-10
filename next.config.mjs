c05a6cefeb171b7466ecd6cf3e16b3b34ebf9597
ducdung121103 <ducdung121103@gmail.com>
2026-06-10 19:33:38 +0700

    Update next.config.mjs and add vercel.json for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -5,12 +5,6 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'image.pollinations.ai',
-      },
-    ],
   },
 }
 
